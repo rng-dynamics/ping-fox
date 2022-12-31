a65d5eff59185e68ebb769739c02480bdf7f9338
Alexander Scharinger <rng.dynamics@gmail.com>
2022-12-31 15:30:17 +0100

    use lcov code coverage report

--- ci_scripts/extract_coverage_value.py
+++ ci_scripts/extract_coverage_value.py
@@ -2,10 +2,16 @@ import os
 import re
 from sys import exit
 
-report_dir = "target/coverage/html"
+report_dir = "target/debug/coverage/"
 
-for line in open(os.path.join(report_dir, "badges/flat.svg"), "r").readlines():
-    if re.search(r"^[ \t]*<title>coverage: [0-9]+%</title>$", line):
-        print(re.findall(r"\b[0-9]+%", line)[0])
-        exit(0)
+with open(os.path.join(report_dir, "index.html"), "r") as file:
+    lines = file.readlines()
+    matching_lines = [
+        line for line in lines if re.search(r"^.*>[0-9]+\.?[0-9]? %</td>.*$", line)
+    ]
+    if not matching_lines:
+        exit(1)
+    print(re.findall(r"\b[0-9]+\.?[0-9]? %", matching_lines[0])[0])
+    exit(0)
+
 exit(1)
