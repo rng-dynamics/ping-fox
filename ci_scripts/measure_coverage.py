0a2b539243b0f40303e44f326c8aa610a8eb20a0
rng-dynamics <73444470+rng-dynamics@users.noreply.github.com>
2023-01-03 16:53:11 +0100

    Add Coveralls

--- ci_scripts/measure_coverage.py
+++ ci_scripts/measure_coverage.py
@@ -4,27 +4,27 @@ import subprocess
 import sys
 from pathlib import Path
 
-destination_dir = "target/coverage/html"
-output_type = "html"
+html_output_dir = "target/debug/coverage/"
 
 
 def create_instrumentation_env():
     env = os.environ.copy()
     env["CARGO_INCREMENTAL"] = "0"
     env["RUSTFLAGS"] = (
+        # " -Zprofile"
+        " -Cinstrument-coverage"
         " -Ccodegen-units=1"
         " -Copt-level=0"
         " -Clink-dead-code"
         " -Coverflow-checks=off"
         " -Zpanic_abort_tests"
         " -Cpanic=abort"
-        " -Cinstrument-coverage"
     )
     env["LLVM_PROFILE_FILE"] = "cargo-test-%p-%m.profraw"
     return env
 
 
-def cargo_test(env):
+def run_coverage(env):
     print("=== run coverage ===")
     subprocess.run(
         ["cargo", "+nightly", "test", "--lib"],
@@ -36,25 +36,68 @@ def cargo_test(env):
     print("ok.")
 
 
-def create_report():
-    print("=== generate report ===")
-    Path(destination_dir).mkdir(parents=True, exist_ok=True)
+def generate_html_report():
+    print("=== generate html report ===")
+    Path(html_output_dir).mkdir(parents=True, exist_ok=True)
     subprocess.run(
         [
             "grcov",
             ".",
-            "--binary-path",
-            "./target/debug/deps",
             "-s",
             ".",
+            "--binary-path",
+            "./target/debug/",
             "-t",
-            output_type,
+            "html",
             "--branch",
             "--ignore-not-existing",
             "--ignore",
             "/*",
             "-o",
-            destination_dir,
+            html_output_dir,
+        ],
+        check=True,
+        stdout=sys.stdout,
+        stderr=sys.stderr,
+    )
+    print("ok.")
+
+
+def generate_report():
+    print("=== generate report ===")
+    Path(html_output_dir).mkdir(parents=True, exist_ok=True)
+    subprocess.run(
+        [
+            "grcov",
+            ".",
+            "-s",
+            ".",
+            "--binary-path",
+            "./target/debug/",
+            "-t",
+            "lcov",
+            "--branch",
+            "--ignore-not-existing",
+            "--ignore",
+            "/*",
+            "-o",
+            "./target/debug/lcov.info",
+        ],
+        check=True,
+        stdout=sys.stdout,
+        stderr=sys.stderr,
+    )
+    subprocess.run(
+        [
+            "genhtml",
+            "-o",
+            html_output_dir,
+            "--show-details",
+            "--highlight",
+            "--ignore-errors",
+            "source",
+            "--legend",
+            "./target/debug/lcov.info"
         ],
         check=True,
         stdout=sys.stdout,
@@ -72,8 +115,8 @@ def cleanup():
 
 if __name__ == "__main__":
     env = create_instrumentation_env()
-    cargo_test(env)
-    create_report()
+    run_coverage(env)
+    generate_report()
     cleanup()
-    print(f"location of code coverage report: {destination_dir}")
+    print(f"location of html code coverage report: {html_output_dir}")
     exit(0)
