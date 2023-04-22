586c31ded2161baa6aa32d489e4210f0ff9dfe26
Alexander Scharinger <rng.dynamics@gmail.com>
2023-04-23 00:28:31 +0200

    add rust documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
+//! A ping (ICMP) library.
+//!
+//! Ping-fox is simple to use and it provides all features without elevated privileges.
+//!
 #![warn(rust_2018_idioms)]
 #![warn(clippy::pedantic)]
 #![allow(clippy::missing_errors_doc)]
+#![warn(missing_docs)]
 
 pub use ping_fox::*;
 pub use ping_receive::*;
--- src/ping_receive.rs
+++ src/ping_receive.rs
@@ -1,18 +1,27 @@
 use std::net::IpAddr;
 use std::time::Duration;
 
+/// Structure representing ping receive cases.
 #[derive(Debug)]
 pub enum PingReceive {
+    /// Case represeting the data from a received echo reply message.
     Data(PingReceiveData),
+    /// Case representing a timeout on an attempt to receive an echo reply message.
     Timeout,
 }
 
+/// Structure represeting a received echo reply message.
 #[derive(Debug)]
 #[allow(clippy::module_name_repetitions)]
 pub struct PingReceiveData {
+    /// The size of the payload in the received reply message.
     pub package_size: usize,
+    /// The IP address of the host which sent the reply.
     pub ip_addr: IpAddr,
+    /// The time to live (TTL) of the received reply message.
     pub ttl: u8,
+    /// The sequence number of the echo reply.
     pub sequence_number: u16,
+    /// The measured duration between sending the echo message and receiving the reply.
     pub ping_duration: Duration,
 }
